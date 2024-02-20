bd31a02010f656cb7cbbcbd3dce88de3849e8bca
Faisal Forever <148379381+FaisalMohammedElorm@users.noreply.github.com>
2024-02-20 21:22:12 +0000

    Add files via upload
    This code depicts how numbers are added in an array

---
+++ Array.js
@@ -0,0 +1,6 @@
+function performAddition(num1, num2, num3, num4, num5, num6, num7, num8, num9){
+  return num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9;
+}
+// Add Array
+const sum = performAddition(1,2,3,4,5,6,7,8,9);
+console.log(sum);
